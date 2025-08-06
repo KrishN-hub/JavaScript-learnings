f4513d72c2cd127124dac3433b2ead6a3b31cfa2
KrishN-hub <krishnakumarpirunthavan@gmail.com>
2025-08-06 15:01:55 +0530

    Refactor calculator functions to use arrow syntax
    Converted sum, sub, mul, and div functions to arrow functions and updated the calculator logic to use the new calc function. Improved input validation and streamlined function calls for better readability and maintainability.

--- class room learnings/06-08-2025/callbackfunctions.js
+++ class room learnings/06-08-2025/callbackfunctions.js
@@ -6,32 +6,23 @@
 // }
 
 // calc(20,20,sum);
-
-function sum(num1,num2) {
- return num1 + num2;
+/*
+function sum(a,b) {
+ return  a+b;
 }
 
-function sub(num1,num2) {
-    return num1 - num2;
+function sub( a,b) {
+    return  a-b;
 }
-function mul(num1,num2){
-    return num1 * num2;
+function mul( a,b) {
+    return  a*b;
 }
-function div(num1,num2){
-    return num1 / num2;
+function div( a,b) {
+    return  a/b;
 }
 
 function calculator(num1,num2, operator){
 
-    if (isNaN(num1)){
-            console.log ( "num1 is not a number");
-            return;
-        }else if( isNaN(num2)){
-            console.log("num2 is not a number");
-            return;
-        }else{
-            num1 = Number(num1);
-            num2 = Number(num2);
             if (isNaN(num1 ) && isNaN(num2)){
                 console.log ( "num1 & num2 are not number");
                 return; 
@@ -50,12 +41,45 @@ function calculator(num1,num2, operator){
                     
                     }
                     }
-                }
+                
                     calculator("name",5, sum);
                     calculator(5,"name",sub);
                     calculator(5,5,mul);
                     calculator(5,0,div);
+            */
+//-----------------------------------------------------------------
+// in arrow function
+sum = (a,b) => a + b;
+sub = (a,b) => a - b;
+mul = (a,b) => a * b;
+div = (a,b) => a / b;
+// calculator function
+calc = (num1,num2, operator) => { 
+    
+    if (isNaN(num1 ) && isNaN(num2)){
+        console.log ( "num1 & num2 are not number");
+        return; 
+    }
+    if (isNaN(num1)){
+            console.log ( "num1 is not a number");
+            return;
+        }else if( isNaN(num2)){
+            console.log("num2 is not a number");
+            return;
+        }else{
+            num1 = Number(num1);
+            num2 = Number(num2);
+            console.log(operator ( num1,num2));
 
+        }
+    }
+
+    calc("name","namr", sum);
+    calc("name",5, sum);
+    calc(5,"name",sub);
+    calc(5,5,mul);
+    calc(5,0,div);
+    
 
 
 
